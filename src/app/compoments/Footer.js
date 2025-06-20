754b15873b38ceb790dd5e23cb3bb07417d2a464
Ken <guest1@Mac.fios-router.home>
2025-06-20 17:37:36 -0400

    Added Footer component and fixed file structure

---
+++ src/app/compoments/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+      <footer style={{
+        textAlign: 'center',
+        padding: '1rem',
+        backgroundColor: '#111',
+        color: '#fff',
+        fontSize: '0.9rem',
+        marginTop: '2rem'
+      }}>
+        © 2025 PyscheTV · All Rights Reserved
+      </footer>
+    );
+  }
+  
