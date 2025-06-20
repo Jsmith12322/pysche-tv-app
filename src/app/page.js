1ddbf905a48e6d1967446e2424fa48ab296fae7d
Ken <guest1@Mac.fios-router.home>
2025-06-20 17:11:20 -0400

    Updated logo with transparent background

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <Image
-        src="/pyschemainlogo.png"
+        src="/pyschenetlogo.png"
         alt="PyscheTV+ Logo"
         width={200}
         height={200}
